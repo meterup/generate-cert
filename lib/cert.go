772b310b29cbf3587a5274913f4a040858be7cd9
Kevin Burke <kevin@meterup.com>
2020-08-05 19:30:24 -0700

    lib: correctly sign leaf certificate
    The leaf must use the private key from the root cert; I think I missed
    this when refactoring. In any event, it works now.

--- lib/cert.go
+++ lib/cert.go
@@ -43,6 +43,7 @@ func Generate(hosts []string, org string, validFor time.Duration) (*Certs, error
 		SerialNumber: serialNumber,
 		Subject: pkix.Name{
 			Organization: []string{org},
+			SerialNumber: serialNumber.String(),
 		},
 		NotBefore: notBefore,
 		NotAfter:  notAfter,
@@ -64,6 +65,7 @@ func Generate(hosts []string, org string, validFor time.Duration) (*Certs, error
 		SerialNumber: leafSerialNumber,
 		Subject: pkix.Name{
 			Organization: []string{org},
+			SerialNumber: leafSerialNumber.String(),
 		},
 		NotBefore: notBefore,
 		NotAfter:  notAfter,
@@ -84,6 +86,7 @@ func Generate(hosts []string, org string, validFor time.Duration) (*Certs, error
 		SerialNumber: clientSerialNumber,
 		Subject: pkix.Name{
 			Organization: []string{org},
+			SerialNumber: clientSerialNumber.String(),
 		},
 		NotBefore: notBefore,
 		NotAfter:  notAfter,
@@ -105,15 +108,15 @@ func Generate(hosts []string, org string, validFor time.Duration) (*Certs, error
 		}
 	}
 
-	root, err := genCert(&rootTemplate, &rootTemplate)
+	root, key, err := genCert(&rootTemplate, &rootTemplate, nil)
 	if err != nil {
 		return nil, err
 	}
-	leaf, err := genCert(&leafTemplate, &rootTemplate)
+	leaf, _, err := genCert(&leafTemplate, &rootTemplate, key)
 	if err != nil {
 		return nil, err
 	}
-	client, err := genCert(&clientTemplate, &rootTemplate)
+	client, _, err := genCert(&clientTemplate, &rootTemplate, key)
 	if err != nil {
 		return nil, err
 	}
@@ -124,35 +127,41 @@ func Generate(hosts []string, org string, validFor time.Duration) (*Certs, error
 	}, nil
 }
 
-func genCert(leaf *x509.Certificate, parent *x509.Certificate) (*Cert, error) {
+func genCert(leaf *x509.Certificate, parent *x509.Certificate, signingKey *ecdsa.PrivateKey) (*Cert, *ecdsa.PrivateKey, error) {
 	key, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
+	}
+	if leaf == parent {
+		if signingKey != nil {
+			return nil, nil, fmt.Errorf("signing key must be nil when generating root cert")
+		}
+		signingKey = key
 	}
 
 	cert := new(Cert)
-	derBytes, err := x509.CreateCertificate(rand.Reader, leaf, parent, &key.PublicKey, key)
+	derBytes, err := x509.CreateCertificate(rand.Reader, leaf, parent, &key.PublicKey, signingKey)
 	if err != nil {
-		return nil, fmt.Errorf("Failed to create certificate: %s", err)
+		return nil, nil, fmt.Errorf("Failed to create certificate: %s", err)
 	}
 	cert.Public = &pem.Block{Type: "CERTIFICATE", Bytes: derBytes}
 	buf := new(bytes.Buffer)
 	if err := pem.Encode(buf, cert.Public); err != nil {
-		return nil, fmt.Errorf("failed to write data to cert.pem: %s", err)
+		return nil, nil, fmt.Errorf("failed to write data to cert.pem: %s", err)
 	}
 	cert.PublicBytes = make([]byte, buf.Len())
 	copy(cert.PublicBytes, buf.Bytes())
 	buf.Reset()
 
-	b, err := x509.MarshalECPrivateKey(key)
+	b, err := x509.MarshalPKCS8PrivateKey(key)
 	if err != nil {
-		return nil, fmt.Errorf("Unable to marshal ECDSA private key: %v", err)
+		return nil, nil, fmt.Errorf("Unable to marshal ECDSA private key: %v", err)
 	}
-	cert.Private = &pem.Block{Type: "EC PRIVATE KEY", Bytes: b}
+	cert.Private = &pem.Block{Type: "PRIVATE KEY", Bytes: b}
 	if err := pem.Encode(buf, cert.Private); err != nil {
-		return nil, fmt.Errorf("failed to encode key data: %s", err)
+		return nil, nil, fmt.Errorf("failed to encode key data: %s", err)
 	}
 	cert.PrivateBytes = make([]byte, buf.Len())
 	copy(cert.PrivateBytes, buf.Bytes())
-	return cert, nil
+	return cert, key, nil
 }
